2474bbbd24bba378cd097e4f77bd14b1c9b5f6f9
haarshjani <harshjani88@gmail.com>
2025-02-13 22:48:32 +0530

    handle the deliminator looks like this: "//[delimiter]\n[numbers…]  failed test

--- tests/numberaddtion.test.js
+++ tests/numberaddtion.test.js
@@ -25,3 +25,8 @@ test('should consider new line between numbers as separator',() => {
 
     expect(addNumbers("1\n2,3")).toBe(6)
 })
+
+test('should hanle the deliminator looks like this: "//[delimiter]\n[numbers…] ', () => {
+
+    expect(addNumbers('//;\n1;2')).toBe(3)
+})
