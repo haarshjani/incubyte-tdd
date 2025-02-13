cc095101e11c899cde15725e41a4860446f6f9ca
haarshjani <harshjani88@gmail.com>
2025-02-13 22:24:50 +0530

    Input: “1”, Output: 1

--- tests/numberaddtion.test.js
+++ tests/numberaddtion.test.js
@@ -5,3 +5,8 @@ test('Should retrun 0 for empty string', () => {
 
     expect(addNumbers("")).toBe(0)
 })
+
+test('should retrun number only if single number in string',() => {
+
+    expect(addNumbers("1")).toBe(1)
+})
